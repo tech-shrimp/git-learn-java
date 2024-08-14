6b753971b9a3d8475968a613330b5e5d45a86ebf
技术爬爬虾 TechShrimp <154193368+tech-shrimp@users.noreply.github.com>
2024-08-14 20:50:55 +0800

    Update HelloDate2.java

--- src/main/java/com/techshrimp/HelloDate2.java
+++ src/main/java/com/techshrimp/HelloDate2.java
@@ -7,5 +7,6 @@ public class HelloDate2 {
     public static void main(String[] args) {
         System.out.println("Hello, it's: ");
         System.out.println(new Date());
+        System.out.println("Hello, techshrimp ");
     }
 }
