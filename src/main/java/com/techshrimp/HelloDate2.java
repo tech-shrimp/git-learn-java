b359fc5e5c1e14c9fe2de50a6b5905c835638bb5
tech-shrimp <techshrimp@163.com>
2024-08-14 20:41:44 +0800

    New file

---
+++ src/main/java/com/techshrimp/HelloDate2.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class HelloDate2 {
+    public HelloDate2() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}
