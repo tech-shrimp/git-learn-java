57c3b49d3093619fa352d30bfb7c901f58898962
tech-shrimp <techshrimp@163.com>
2024-08-16 12:32:24 +0800

    第四部分

--- src/main/java/com/techshrimp/HelloDate3.java
+++ src/main/java/com/techshrimp/HelloDate3.java
@@ -7,7 +7,7 @@ public class HelloDate3 {
     public static void main(String[] args) {
         System.out.println("Hello, it's: ");
         System.out.println(new Date());
-        System.out.println("第一部分");
+        System.out.println("第二部分");
         System.out.println("第二部分");
         System.out.println("第三部分");
         System.out.println("第四部分");
