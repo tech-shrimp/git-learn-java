259570fe3990b229a724e4d1eaf1e06e566da79e
tech-shrimp <techshrimp@163.com>
2024-08-17 11:02:49 +0800

    fourth part

--- src/main/java/com/techshrimp/HelloDate3.java
+++ src/main/java/com/techshrimp/HelloDate3.java
@@ -10,6 +10,6 @@ public class HelloDate3 {
         System.out.println("frist part");
         System.out.println("第二部分");
         System.out.println("第三部分");
-        System.out.println("第四部分");
+        System.out.println("fourth part");
     }
 }
