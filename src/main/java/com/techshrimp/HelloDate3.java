750fe2991a0c1586cf76608024b95df8f5f6fd41
tech-shrimp <techshrimp@163.com>
2024-08-19 16:27:30 +0800

    1

--- src/main/java/com/techshrimp/HelloDate3.java
+++ src/main/java/com/techshrimp/HelloDate3.java
@@ -7,5 +7,6 @@ public class HelloDate3 {
     public static void main(String[] args) {
         System.out.println("Hello, it's: ");
         System.out.println(new Date());
+        System.out.println("Hello, it's: 1");
     }
 }
